e6e024c605301cd39f81bbf0be00cad01c42ca8b
Vijay Kumar Uppala <vijaykumar.uppala@xchanging.my.local>
2018-11-03 22:45:15 +0800

    Updated 1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,7 +14,7 @@ pipeline{
         }
         stage('Deploy to Staging'){
             steps{
-                    build job: 'deploy-to-staging'
+                   echo 'Now Deploying..'
                 }
         }
     }
