835bc4ce8e7fab8ef94e72d1bc5d13cb778d262d
Vijay Kumar Uppala <vijaykumar.uppala@xchanging.my.local>
2018-11-04 16:04:29 +0800

    echo updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,7 +13,7 @@ pipeline {
 stages{
         stage('Build'){
             steps {
-                sh 'mvn clean package'
+                echo 'mvn clean package'
             }
             post {
                 success {
