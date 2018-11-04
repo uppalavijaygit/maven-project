14f55faca6c3b1816c95ff8ddfefeb4208abec4b
Vijay Kumar Uppala <vijaykumar.uppala@xchanging.my.local>
2018-11-04 16:31:24 +0800

    SCP

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,13 +27,13 @@ stages{
             parallel{
                 stage ('Deploy to Staging'){
                     steps {
-                        bat "winscp -i V:/Program Files/Servers/NewKeyPair.pem **/target/*.war ec2-user@${params.tomcat_dev}:/opt/apache-tomcat-7.0.91/webapps"
+                        bat "scp -i V:/Program Files/Servers/NewKeyPair.pem **/target/*.war ec2-user@${params.tomcat_dev}:/opt/apache-tomcat-7.0.91/webapps"
                     }
                 }
 
                 stage ("Deploy to Production"){
                     steps {
-                        bat "winscp -i V:/Program Files/Servers/NewKeyPair.pem **/target/*.war ec2-user@${params.tomcat_prod}:/opt/apache-tomcat-7.0.91/webapps"
+                        bat "scp -i V:/Program Files/Servers/NewKeyPair.pem **/target/*.war ec2-user@${params.tomcat_prod}:/opt/apache-tomcat-7.0.91/webapps"
                     }
                 }
             }
