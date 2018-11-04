09e7e8ae05517ca5c120823d54474c8b4f49df88
Vijay Kumar Uppala <vijaykumar.uppala@xchanging.my.local>
2018-11-04 15:37:30 +0800

    Kenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,4 @@
 pipeline{
     agent any
-    stages{
-        stage('Build'){
-                steps{
-                    bat 'mvn package'
-                }
-                post{
-                    success{
-                        echo 'Now Archving..'
-                        archiveArtifacts artifacts: '**/target/*.war'
-                    }
-                }
-        }
-        stage('Deploy to Staging'){
-            steps{
-                   echo 'Now Deploying..'
-                }
-        }
-    }
+    
 }
