30717673b922c22f230184a4d6069cca64cbbf10
Vijay Kumar Uppala <vijaykumar.uppala@xchanging.my.local>
2018-11-04 22:48:53 +0800

    asdasd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,7 +27,7 @@ stages{
             parallel{
                 stage ('Deploy to Staging'){
                     steps {
-                         bat "pscp -scp -i "V:/Program Files/Servers/keyGen.ppk" "C:\\Program Files (x86)\\Jenkins\\workspace\\maven-project\\webapp\\target\\webapp.war" ec2-user@${params.tomcat_dev}:/opt/apache-tomcat-7.0.91/webapps"
+                         bat "pscp -scp -i V:\\Program Files\\Servers\\keyGen.ppk C:\\Program Files (x86)\\Jenkins\\workspace\\maven-project\\webapp\\target\\webapp.war ec2-user@${params.tomcat_dev}:/opt/apache-tomcat-7.0.91/webapps"
                     }
                 }
 
